6df40ac03d422a461b9bc635cca26cf39937f976
Alexander-ls <fabitor_16@hotmail.com>
2025-06-09 19:58:39 -0500

    add logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import LoginForm from '../components/LoginForm';
-import logo from '../assets/barber.svg';
+import logo from '../assets/barber.png';
 
 export default function Home() {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col w-full max-w-md">
-        <img src={logo} alt="Logo Barbería" className="w-32 mb-4" />
+        <img src={logo} alt="Logo Barbería" className="w-32 mb-4 logo-main" />
         <h1 className="text-3xl font-bold mb-4 text-center">Bienvenido a la Barbería</h1>
         <div className="card w-full shadow-2xl bg-base-100">
           <div className="card-body">

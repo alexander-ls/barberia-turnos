5ad84ad7be7705e660a39f9cc776078374034027
Alexander-ls <fabitor_16@hotmail.com>
2025-07-01 15:30:26 -0500

    add img

--- src/components/BarberMisTurnos.jsx
+++ src/components/BarberMisTurnos.jsx
@@ -137,6 +137,9 @@ export default function BarberMisTurnos() {
       </div>
 
       <div role="alert" className="alert alert-info justify-center">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="h-6 w-6 shrink-0 stroke-current">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+  </svg>
         <span>
           Tienes {turnosDelDia.length} turnos hoy. Pendientes: {pendientesCount}
           {' \u2013 '}Atendidos: {atendidosCount}
